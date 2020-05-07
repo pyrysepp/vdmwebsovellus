803f0743f28710c88729e8c59e0c3f73567c112d
Viliina Nyyssönen <viliina.nyyssonen@aalto.fi>
2020-05-07 14:53:44 +0300

    tekstisivujen muotoilua aloitettu

--- src/components/Ympäristö.js
+++ src/components/Ympäristö.js
@@ -3,7 +3,9 @@ import './Yleinen.css'
 
 const Ympäristö = () => {
     return(
-        <div className="text-container">
+        <div className="container">
+            <div className="content">
+            <div className="text-container">
                 <h2>Ydinvoimaloiden elinkaaripäästöt</h2>
 
                 <p>Ydinvoimaloiden elinkaaripäästöt vertautuvat tuulivoimaan, sillä vähäiset kasvihuonekaasu päästöt aiheutuvat ydinvoiman tuotantoketjussa materaalien ja polttoaineiden hankinnasta, laitevalmistuksesta, kuljetuksista ja laitosten rakentamisesta ja purkamisesta. Ydinvoimalla voidaan tuottaa suuri määriä energiaa todella pienillä hiilidioksidipäästöillä, ja siksi se on hyvä vaihtoehto pyrkimyksissä kohti dramaattisia toimia vaativia ilmastotavoitteita. 
@@ -23,6 +25,8 @@ const Ympäristö = () => {
 
                 <p>Ydinvoimalaitoksista ympäristöön, esimerkiksi ilmaan ja veteen, joutuville päästöille on asetettu päästörajat. Päästöjä valvotaan säännöllisesti mittauksilla ydinvoimalaitosten ympäristöstä erilaisten säteilyannos- ja radioaktiivisuusmittausten avulla. Suomessa ydinvoimalaitosten ympäristövaikutukset ovat koko käyttöajan pieniä, ja päästöt vain murto-osan sallituista arvoista. Sallittu radioaktiivisten aineiden päästö ympäristöön määritellään siten, että kukaan laitoksen lähistöllä asuva ei voi saada päästöjen aiheuttamana suurempaa kuin 0,1 millisievertin säitelyannosta vuodessa. Suomessa nämä säitelyannokset ovat kuitenkin olleet vain muutaman prosentin sallitusta raja-arvosta, ja esimerkiksi luonnon taustasäteily aiheuttaa suomalaisille keskimäärin 3,7 millisievertin säitelyannoksen vuodessa. 
                 </p>
+            </div>
+            </div>
         </div>
     )
 }
