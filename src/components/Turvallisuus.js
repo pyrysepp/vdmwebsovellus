2c5e7f8904b636b1d04d9eec1edfdefb6e1b0990
Iida Lehikoinen <iida.lehikoinen@aalto.fi>
2020-05-08 11:00:26 +0300

    lisätty sisältöä ja css-säätöä

--- src/components/Turvallisuus.js
+++ src/components/Turvallisuus.js
@@ -4,8 +4,46 @@ import React from 'react'
 const Turvallisuus = () => {
 
     return(
-        <div>
-         
+        <div className="yleis-container">
+            <div className="content">
+            <div className="text-container">
+                <h2>Ydinvoiman turvallisuus Suomessa</h2>
+
+                <p>Suomessa ydinvoimaloiden toimintaa valvotaan ja säädetään tarkasti, jonka takia ydinvoimaloiden käyttäminen on turvallista. Ydinenergialaki, jossa on erilaisia määräyksiä ydinvoimaloiden toiminnasta, turvallisuusmekanismeista sekä riskeihin varautumisesti, sekä Säteilyturvakeskus, joka valvoo ydinvoimaloiden turvallisuutta sekä ydinenergialain toteutumista, takaavat ydinenergian käytön tuvallisuuden Suomessa. 
+                </p>
+            </div>
+            <div className="text-container">
+                <h2>Ydinvoimaloiden turvallisuusmääräykset sekä valvonta</h2>
+
+                <p>
+                Suomessa on ydinenergialaki, jossa annetaan määräyksiä esimerkiksi ydinvoiman käytön vaikutuksista yhteiskuntaan sekä ympäristöön ja ydinvoimaloissa vaadittavista suojausmekanismeista sekä käyttöhäiriöihin sekä onnettomuuksiin varautumisesta sekä niiden estämisestä. Laissa myös määrätään, että ydinvoimaloiden on osoitettava näiden vaatimusten täyttyminen, ja turvallisuutta pitää arvioida kokonaisuutena vähintään 10 vuoden välein. 
+                <br/> <br/>
+                Ydinvoimaloiden turvallisuuden valvonnasta Suomessa vastaa Säteilyturvakeskus (STUK). Suomessa luvanhaltijalla on vastuu noudattaa turvallisuusmääräyksiä, ja STUK varmistaa valvonnallaan, että kaikkia määräyksiä noudatetaan. Ydinmateriaalivalvonnalla eli ydinaseiden leviämisen estämiseksi tarkoitetulla valvonnalla varmistutaan siitä, että ydinaineet ja muut ydinalan tuotteet pysyvät rauhanomaisessa, lupien ja ilmoitusten mukaisessa käytössä ja että ydinlaitoksia ja alan tekniikkaa käytetään vain rauhanomaisiin tarkoituksiin.
+                </p>
+            </div>
+            <div className="text-container">
+                <h2>Ydinjäte</h2>
+                <p>
+                Ydinjätteistä on lain mukaan huolehdittava loppusijoitusta myöten Suomessa.  Säteilyturvakeskus valvoo ydinjätteen käsittelyn, varastoinnin ja loppusijoituksen turvallisuutta, mutta vastuu sen hoitamisesta on ydinvoimaloiden käyttäjillä. Käytetty ydinpolttoaine varastoidaan Suomessa ydinvoimalaitospaikoilla sijaitsevissa vesiallasvarastoissa väliaikaisesti. Suomessa on rakennettu ydinjätteiden loppusijoituspaikkaa, jonka käytön olisi tarkoitus alkaa vuonna 2020. Ydinenergialaki sanoo, että Suomessa syntyvä ydinjäte on loppusijoitettava Suomeen. Näin varmistutaan siitä, että ydinjätteet käsitellään asiaan kuuluvalla tavalla.
+                </p>
+            </div>
+            <div className="text-container">
+                <h2>Varautuminen ongelmiin</h2>
+                <p>
+                Ydinvoimaloissa pyritään ehkäisemään erilaisia ongelmatilanteita, jotta mahdollisilta vaaratilanteilta pystyttäisiin välttymään mahdollisimman tehokkaasti. Korkeatasoisesta laadunvarmistuksesta huolimatta ydinvoimalaitoksillakin esiintyy laitevikoja ja käyttöhenkilökunnan virheitä. Tämä on otettu huomioon laitosten suunnittelussa, ja yksittäisten vikojen ja virheiden kehittyminen onnettomuuksiksi on estetty luotettavasti. Laitosten suunnittelussa ja käytössä on varauduttu myös onnettomuustilanteisiin, ja niiden varalta laitoksissa on erityisiä turvallisuusjärjestelmiä. 
+                <br/> <br/> 
+                Ydinvoimaloidenkin turvallisuutta kehitetään koko ajan, ja esimerkiksi rakenteilla oleva Olkuiluoto 3 -ydinvoimala on varautunut terrori-iskuihin ja sabotaasiin siten, että tällaisissa tilanteissa pystytään ehkäisemään suuria onnettomuuksia ja haittaa ympäristölle. 
+                </p>
+            </div>
+            <div className="text-container">
+                <h2>Lue lisää</h2>
+                <p>
+                    <a href="https://www.finlex.fi/fi/laki/ajantasa/1987/19870990">Ydinenergialaki</a>
+                    <br/> <br/>
+                    <a href="https://www.stuk.fi/stuk-valvoo/ydinturvallisuus">Ydinturvallisuus (STUK)</a>
+                </p>
+            </div>
+            </div>
         </div>
     )
 }
